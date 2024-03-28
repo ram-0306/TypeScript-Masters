f5036b3c24488063a670e6e182f271c1c5cc5371
Ramgopal Gautam <ramgopalgautam456@gmail.com>
2024-03-28 13:51:48 +0530

    model js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,12 @@
 const {Schema, model} = require("../connection")
 
 const myschema = new Schema({
-    name: String,
-    email: String,
+    name: {type:String, require:true},
+    email: {type:String, require:true, unique:true},
+    password:{String},
+    avatar:{type :String ,default:'avtarPlaceholder.png' },
+    role:{type:String , default:'user'},
+    createdAt:{Date},
 });
 
-module.exports = model("user", myschema)
+module.exports = model("users", myschema);
