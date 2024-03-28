24081e1d5bc2fa3e1b9a96cf0541746cafcda564
Ramgopal Gautam <ramgopalgautam456@gmail.com>
2024-03-28 11:16:02 +0530

    2

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -1,52 +1,44 @@
 'use client';
 import { Paper, Text, TextInput, Textarea, Button, Group, SimpleGrid, Container } from '@mantine/core';
 import ContactIconsList from './contactIcon';
-import bg from './bg.svg';
 import classes from './contact.module.css';
 
-const Contact = () =>  {
+const Contact = () => {
   return (
     <Container>
-    <Paper shadow="md" radius="lg">
-      <div className={classes.wrapper}>
-        <div className={classes.contacts} style={{ backgroundImage: `url(${bg.src})` }}>
+      <Paper shadow="md" radius="lg" style={{ position: 'relative', zIndex: 1 }}>
+        <div className={classes.wrapper}>
           <Text fz="lg" fw={700} className={classes.title} c="#fff">
             Contact information
           </Text>
-
           <ContactIconsList />
+          <form className={classes.form} onSubmit={(event) => event.preventDefault()}>
+            <Text fz="lg" fw={700} className={classes.title}>
+              Get in touch
+            </Text>
+            <div className={classes.fields}>
+              <SimpleGrid cols={{ base: 1, sm: 2 }}>
+                <TextInput label="Your name" placeholder="Your name" />
+                <TextInput label="Your email" placeholder="hello@mantine.dev" required />
+              </SimpleGrid>
+              <TextInput mt="md" label="Subject" placeholder="Subject" required />
+              <Textarea
+                mt="md"
+                label="Your message"
+                placeholder="Please include all relevant information"
+                minRows={3}
+              />
+              <Group justify="flex-end" mt="md">
+                <Button type="submit" className={classes.control}>
+                  Send message
+                </Button>
+              </Group>
+            </div>
+          </form>
         </div>
-
-        <form className={classes.form} onSubmit={(event) => event.preventDefault()}>
-          <Text fz="lg" fw={700} className={classes.title}>
-            Get in touch
-          </Text>
-
-          <div className={classes.fields}>
-            <SimpleGrid cols={{ base: 1, sm: 2 }}>
-              <TextInput label="Your name" placeholder="Your name" />
-              <TextInput label="Your email" placeholder="hello@mantine.dev" required />
-            </SimpleGrid>
-
-            <TextInput mt="md" label="Subject" placeholder="Subject" required />
-
-            <Textarea
-              mt="md"
-              label="Your message"
-              placeholder="Please include all relevant information"
-              minRows={3}
-            />
-
-            <Group justify="flex-end" mt="md">
-              <Button type="submit" className={classes.control}>
-                Send message
-              </Button>
-            </Group>
-          </div>
-        </form>
-      </div>
-    </Paper>
+      </Paper>
     </Container>
   );
-}
+};
+
 export default Contact;
