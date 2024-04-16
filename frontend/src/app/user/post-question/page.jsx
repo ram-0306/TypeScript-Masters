03bf419243ae7663eabdc9f7ebdf946171c0c1be
Ayush Singh <ayushsingh723585@gmail.com>
2024-04-16 16:13:34 +0530

    post-question

--- frontend/src/app/user/post-question/page.jsx
+++ frontend/src/app/user/post-question/page.jsx
@@ -1,4 +1,11 @@
 'use client'
+import EditorJS from "@editorjs/editorjs";
+import Header from '@editorjs/header';
+import List from '@editorjs/list';
+import SimpleImage from '@editorjs/image';
+import Checklist from '@editorjs/checklist';
+import Quote from '@editorjs/quote';
+import Table from '@editorjs/table';
 import {
   Paper,
   Title,
@@ -11,6 +18,96 @@ import {
 import { enqueueSnackbar } from 'notistack';
 import { useForm } from '@mantine/form';
 
+
+const EditorComponent = () => {
+  const ejInstance = useRef();
+
+  const [title, setTitle] = useState('');
+
+  const { id } = useParams();
+  // console.log(id);
+  const [guideData, setGuideData] = useState(null);
+  const [initialContent, setInitialContent] = useState(DEFAULT_INITIAL_DATA);
+
+  const initEditor = () => {
+    const editor = new EditorJS({
+      holder: 'editorjs',
+      onReady: () => {
+        ejInstance.current = editor;
+      },
+      autofocus: true,
+      // data: DEFAULT_INITIAL_DATA,
+      onChange: async () => {
+        let content = await editor.saver.save();
+
+        console.log(content);
+      },
+      data: guideData.content ?? initialContent,
+      tools: {
+        header: Header,
+        list: List,
+        image: SimpleImage,
+        checklist: Checklist,
+        quote: Quote,
+        table: Table
+        
+      },
+    });
+  };
+
+
+  useEffect(() => {
+    if (ejInstance.current === null && guideData !== null) {
+      initEditor();
+    }
+
+    return () => {
+      ejInstance?.current?.destroy();
+      ejInstance.current = null;
+    };
+  }, [guideData]);
+
+
+
+  const updateGuide = () => {
+   
+    ejInstance.current.save()
+      .then((result) => {
+        console.log(result);
+        fetch('http://localhost:5000/guide/update/' + id, {
+          method: 'PUT',
+          body: JSON.stringify({
+            content: result,
+            title
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then((response) => {
+          console.log(response.status);
+          enqueueSnackbar('Guide Updated Successfully', { variant: 'success' });
+        })
+      }).catch((err) => {
+        console.log(err);
+      });
+
+
+    // return 
+
+  }
+
+  return <>
+
+    <TextInput label="Guide Title" value={title} onChange={e => setTitle(e.target.value)} />
+    <Button justify="center" leftSection={<IconPlus />} variant="gradient"
+      gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+      onClick={updateGuide}
+    >
+      Update
+    </Button>
+    <div id='editorjs'></div></>
+}
 
 const postQuestion = () => {
   const quesForm = useForm({
